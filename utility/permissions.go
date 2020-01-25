7a5507fbbfd91d65bad8d624fac1ca92b0f4ff07
koderholic <oluchi@bundle.com>
2020-01-25 13:28:42 +0100

    updated permissions

--- utility/permissions.go
+++ utility/permissions.go
@@ -2,13 +2,11 @@ package utility
 
 var (
 	Permissions = map[string]string{
-		"GetUserAssets":         "get-assets",
-		"CreateUserAssets":      "create-assets",
-		"CreditUserAsset":       "credit-asset",
-		"DebitUserAsset":        "debit-asset",
-		"InternalTransfer":      "do-internal-transfer",
-		"GetAssetAddress":       "get-address",
-		"GetUserAssetById":      "get-asset-byid",
-		"GetUserAssetByAddress": "get-asset-byaddr",
+		"GetUserAssets":    "get-assets",
+		"CreateUserAssets": "create-assets",
+		"CreditUserAsset":  "credit-asset",
+		"DebitUserAsset":   "debit-asset",
+		"InternalTransfer": "do-internal-transfer",
+		"GetAssetAddress":  "get-address",
 	}
 )
