f70f18b3cfed1f34185f36964a9f5aeeaf7c55de
koderholic <oluchi@bundle.com>
2020-02-20 19:59:10 +0100

    fix build err

--- test/services_test.go
+++ test/services_test.go
@@ -19,6 +19,7 @@ const (
 	}`
 )
 
+
 func TestSignTransactionImplementation(t *testing.T) {
 
 	logger := utility.NewLogger()
@@ -27,6 +28,7 @@ func TestSignTransactionImplementation(t *testing.T) {
 		ServiceName:           "crypto-wallet-adapter",
 		AuthenticatorKey:      "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUE0ZjV3ZzVsMmhLc1RlTmVtL1Y0MQpmR25KbTZnT2Ryajh5bTNyRmtFVS93VDhSRHRuU2dGRVpPUXBIRWdRN0pMMzh4VWZVMFkzZzZhWXc5UVQwaEo3Cm1DcHo5RXI1cUxhTVhKd1p4ekh6QWFobGZBMGljcWFidkpPTXZRdHpENnVRdjZ3UEV5WnREVFdpUWk5QVh3QnAKSHNzUG5wWUdJbjIwWlp1TmxYMkJyQ2xjaUhoQ1BVSUlaT1FuL01tcVREMzFqU3lqb1FvVjdNaGhNVEFUS0p4MgpYckhoUisxRGNLSnpRQlNUQUducFlWYXFwc0FSYXArbndSaXByM25VVHV4eUdvaEJUU21qSjJ1c1NlUVhISTNiCk9ESVJlMUF1VHlIY2VBYmV3bjhiNDYyeUVXS0FSZHBkOUFqUVc1U0lWUGZkc3o1QjZHbFlRNUxkWUt0em5UdXkKN3dJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t",
 		PurgeCacheInterval:    5,
+		ExpireCacheDuration:   400,
 		ServiceID:             "4b0bde7a-9201-4cf9-859f-e61d976e376d",
 		ServiceKey:            "32e1f6396de342e879ca07ec68d4d907",
 		AuthenticationService: "https://internal.dev.bundlewallet.com/authentication",
@@ -41,6 +43,10 @@ func TestSignTransactionImplementation(t *testing.T) {
 	}
 	responseData := model.SignTransactionResponse{}
 	serviceErr := model.ServicesRequestErr{}
+
+	purgeInterval := Config.PurgeCacheInterval * time.Second
+	cacheDuration := Config.ExpireCacheDuration * time.Second
+	authCache := utility.InitializeCache(cacheDuration, purgeInterval)
 
 	if err := services.SignTransaction(authCache, logger, Config, requestData, &responseData, serviceErr); err == nil {
 		t.Errorf("Expected SignTransaction to error due to validation on request data, got %s\n", err)
@@ -65,6 +71,7 @@ func TestBatchSignBtcImplementation(t *testing.T) {
 		AuthenticationService: "https://internal.dev.bundlewallet.com/authentication",
 		KeyManagementService:  "https://internal.dev.bundlewallet.com/key-management",
 	}
+
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//assert.Equal(t, "key", r.Header.Get("Key"))
 		//assert.Equal(t, "secret", r.Header.Get("Secret"))
@@ -94,10 +101,12 @@ func TestBatchSignBtcImplementation(t *testing.T) {
 	}
 	signTransactionResponse := model.SignTransactionResponse{}
 	serviceErr := model.ServicesRequestErr{}
+
 	purgeInterval := Config.PurgeCacheInterval * time.Second
 	cacheDuration := Config.ExpireCacheDuration * time.Second
 	authCache := utility.InitializeCache(cacheDuration, purgeInterval)
-	if err := services.SignBatchBTCTransaction(httpClient, authCache, logger, Config, signTransactionRequest, &signTransactionResponse, serviceErr); err != nil {
+
+	if err := services.SignBatchBTCTransaction(httpClient, authCache, logger, Config, signTransactionRequest, &signTransactionResponse, serviceErr); err == nil {
 		t.Errorf("Expected SignTransaction to error due to validation on request data, got %s\n", err)
 	}
 
@@ -125,6 +134,10 @@ func TestBroadcastTransactionImplementation(t *testing.T) {
 	responseData := model.BroadcastToChainResponse{}
 	serviceErr := model.ServicesRequestErr{}
 
+	purgeInterval := Config.PurgeCacheInterval * time.Second
+	cacheDuration := Config.ExpireCacheDuration * time.Second
+	authCache := utility.InitializeCache(cacheDuration, purgeInterval)
+	
 	if err := services.BroadcastToChain(authCache, logger, Config, requestData, &responseData, serviceErr); err == nil {
 		t.Errorf("Expected SignTransaction to error due to incorrect signed data, got %s\n", err)
 	}
