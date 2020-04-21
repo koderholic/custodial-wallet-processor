f51086002ee50b841947009aac95e64621e46e04
koderholic <oluchi@bundle.com>
2020-04-21 23:49:48 +0100

    added support for BUSD

--- services/hotWalletInitService.go
+++ services/hotWalletInitService.go
@@ -13,23 +13,43 @@ import (
 func InitHotWallet(cache *utility.MemoryCache, DB *gorm.DB, logger *utility.Logger, config Config.Data) error {
 
 	supportedAssets := []dto.Denomination{}
+	coinTypeToAddrMap := map[int64]string{}
+	externalServiceErr := model.ServicesRequestErr{}
+	serviceID, _ := uuid.FromString(config.ServiceID)
+	address := ""
+	var err error
 
-	if err := DB.Find(&supportedAssets).Error; err != nil {
+	if err := DB.Order("created_at", true).Find(&supportedAssets).Error; err != nil {
 		if err.Error() != utility.SQL_404 {
 			return err
 		}
 	}
 
 	for _, asset := range supportedAssets {
-		address, err := GetHotWalletAddressFor(cache, DB, logger, config, asset.AssetSymbol)
+
+		address, err = GetHotWalletAddressFor(cache, DB, logger, config, asset.AssetSymbol)
 		if err != nil {
 			logger.Error("Error with getting hot wallet address for %s : %s", asset.AssetSymbol, err)
 			return err
 		}
+
 		if address != "" {
-			if err := DB.Create(&dto.HotWalletAsset{Address: address, AssetSymbol: asset.AssetSymbol}).Error; err != nil {
-				logger.Error("Error with creating hot wallet asset record %s : %s", asset.AssetSymbol, err)
+			coinTypeToAddrMap[asset.CoinType] = address
+			continue
+		}
+
+		if coinTypeToAddrMap[asset.CoinType] != "" {
+			address = coinTypeToAddrMap[asset.CoinType]
+		} else {
+			address, err = GenerateAddress(cache, logger, config, serviceID, asset.AssetSymbol, &externalServiceErr)
+			if err != nil {
+				return err
 			}
+			coinTypeToAddrMap[asset.CoinType] = address
+		}
+
+		if err := DB.Create(&dto.HotWalletAsset{Address: address, AssetSymbol: asset.AssetSymbol}).Error; err != nil {
+			logger.Error("Error with creating hot wallet asset record %s : %s", asset.AssetSymbol, err)
 		}
 
 	}
@@ -41,19 +61,13 @@ func InitHotWallet(cache *utility.MemoryCache, DB *gorm.DB, logger *utility.Logg
 // GetHotWalletAddressFor ... Get the Bundle hot wallet address corresponding to a certain asset
 func GetHotWalletAddressFor(cache *utility.MemoryCache, DB *gorm.DB, logger *utility.Logger, config Config.Data, asseSymbol string) (string, error) {
 	hotWallet := dto.HotWalletAsset{}
-	externalServiceErr := model.ServicesRequestErr{}
-	serviceID, _ := uuid.FromString(config.ServiceID)
 
 	if err := DB.Where(dto.HotWalletAsset{AssetSymbol: asseSymbol}).First(&hotWallet).Error; err != nil {
 		if err.Error() != utility.SQL_404 {
 			return "", err
 		}
-		address, err := GenerateAddress(cache, logger, config, serviceID, asseSymbol, &externalServiceErr)
-		if err != nil {
-			return "", err
-		}
-		return address, nil
+		return "", nil
 	}
 
-	return "", nil
+	return hotWallet.Address, nil
 }
