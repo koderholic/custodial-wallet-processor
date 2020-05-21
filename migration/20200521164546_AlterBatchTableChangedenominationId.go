03b164ad4f974d445f7c9966e349d7c920c9fa12
koderholic <oluchi@bundle.com>
2020-05-21 20:43:27 +0100

    changed default for status on batch_requests from PENDING to AWAITING_TRANSACTIONS

--- migration/20200521164546_AlterBatchTableChangedenominationId.go
+++ migration/20200521164546_AlterBatchTableChangedenominationId.go
@@ -15,7 +15,7 @@ func Up20200521164546(tx *sql.Tx) error {
 	if err != nil {
 		 return err
 	}
-	_, err := tx.Exec("ALTER TABLE batch_requests Change status status VARCHAR(100) NOT NULL DEFAULT 'AWAITING_TRANSACTIONS';")
+	_, err = tx.Exec("ALTER TABLE batch_requests Change status status VARCHAR(100) NOT NULL DEFAULT 'AWAITING_TRANSACTIONS';")
 	if err != nil {
 		 return err
 	}
@@ -28,7 +28,7 @@ func Down20200521164546(tx *sql.Tx) error {
 	if err != nil {
 		 return err
 	}
-	_, err := tx.Exec("ALTER TABLE batch_requests Change status status VARCHAR(100)  NOT NULL DEFAULT 'PENDING';")
+	_, err = tx.Exec("ALTER TABLE batch_requests Change status status VARCHAR(100)  NOT NULL DEFAULT 'PENDING';")
 	if err != nil {
 		 return err
 	}
