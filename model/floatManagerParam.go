17ae88c87db96f5b02755c55f8d7331a5cb537c2
Oluchi Enebeli <meetenebelioluchi@gmail.com>
2020-06-18 19:10:15 +0100

    Bundle:1207-float manager improvement (#173)
    * update float variables
    
    * updated float variables
    
    * increased minimum trigger percentage for float
    
    * increased minimum trigger percentage for float
    
    * update PR
    
    * updated PR, cleaned up float manager
    
    * updated PR
    
    Co-authored-by: koderholic <oluchi@bundle.com>

---
+++ model/floatManagerParam.go
@@ -0,0 +1,13 @@
+package model
+
+// FloatManagerParam...
+type FloatManagerParam struct {
+	BaseModel
+	MinPercentMaxUserBalance       float64
+	MaxPercentMaxUserBalance       float64
+	MinPercentTotalUserBalance     float64
+	AveragePercentTotalUserBalance float64
+	MaxPercentTotalUserBalance     float64
+	PercentMinimumTriggerLevel     float64
+	PercentMaximumTriggerLevel     float64
+}
